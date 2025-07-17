aedd406c3c407c0eb0fd943ae8becc27bb75fdbb
SHUBHAM RANA <shubhamghost1234@gmail.com>
2025-07-17 11:21:03 +0530

    the kuki file is committed

---
+++ kuki.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>javascript</title>
+<script type="text/javascript" language="javascript">
+var cust_name
+cust_name=prompt("Enter the name")
+alert("customer name is " + cust_name)
+cust_name=prompt("change the customer name")
+alert("customer name is " + cust_name)
+</script>
+</head>
+</html>
